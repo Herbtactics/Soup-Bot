78f90c15f4f82a8e96e3972345794581db21e4fb
Norman Chan <herbtactics@gmail.com>
2022-09-13 23:04:55 -0700

    fuck yml didn't need it i think

--- SoupBot.py
+++ SoupBot.py
@@ -1,9 +1,11 @@
 import discord   
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
-bot_TKN = os.environ['BOT_TOKEN'] # or bot_TKN = os.environ.get('BOT_TOKEN')
-channel_ID = os.environ['CHANNELID']
+load_dotenv()
+
+bot_TKN = os.getenv('BOT_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
