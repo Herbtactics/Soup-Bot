f634ae7b9d47cffcd34d651c729340d22b794882
Norman Chan <herbtactics@gmail.com>
2022-09-13 22:00:32 -0700

    add gitignore

--- SoupBot.py
+++ SoupBot.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv # for loading stuff from the .env file
 
 load_dotenv()
 
-bot_TKN = os.getenv('BOT_TOKEN')
-CHANNELID = os.getenv('_CHANNELID')
+bot_TKN = os.environ('BOT_TOKEN')
+channel_ID = os.environ('_CHANNELID')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,6 +17,8 @@ client = discord.Client(intents=intents)
 @client.event # .event is a decorator which listens to when discord opens, aka the 'client' var             
 async def on_ready(): # on_ready is a api function tht signals tht a connection with discord has been made  
     print(f'{client.user} has connected to Discord!') # f'{}' has same function as .format
+    print(bot_TKN)
+    print(channel_ID)
 
 # @client.event
 # @commands.has_permissions()
