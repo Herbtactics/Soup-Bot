4c84c502a4b0a4abfb2f674f479d4175998cd0ce
Norman Chan <herbtactics@gmail.com>
2022-09-14 17:59:51 -0700

    test phase

--- Selenium.py
+++ Selenium.py
@@ -2,30 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 import urllib
 import urllib.request
 import os
 import pyautogui
+from knownpaths import *
 
 # What does soup bot do:
 
 # Take inputs from discord chat (i.e. chicken, noddle)
-string = ""
+# string = ""
 
-# Add inputs into a list
-input_list = []
-input_list.append(string)
+# # Add inputs into a list
+# input_list = []
+# input_list.append(string)
 
-# Compile them into a single string variable
-single_string = ""
-for index in range(0, len(list)):
-    single_string += (list[index] + " ")
+# # Compile them into a single string variable
+# single_string = ""
+# for index in range(0, len(list)):
+#     single_string += (list[index] + " ")
 
 # Put the variable into a Google images search tab
     # Opening Google on Chrome browser
-PATH = r"C:\Users\steam\Desktop\chromedriver.exe"
+
+def find(name,path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+PATH = find("chromedriver.exe", 'C:\\')
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.google.com/imghp?hl=en&ogbl")
 driver.maximize_window()
@@ -35,7 +45,7 @@ search = driver.find_element(By.NAME, "q")
 search.send_keys("chicken noodle soup") # Replace with single_string
 search.send_keys(Keys.RETURN)
 
-# Takes a random image and selects it
+# Takes a random image and gets its url
 picture_number = random.randint(1, 10)
 img = driver.find_elements(By.CLASS_NAME, "rg_i")
 img_num = 0
@@ -44,19 +54,38 @@ if len(img) < 10:
 else:
     img_num = random.randint(0, 9)
 img[img_num].click()
+test = []
+img_jpg = WebDriverWait(driver, 40).until(EC.visibility_of_element_located((By.CLASS_NAME, "v4dQwb")))
+for img in img_jpg:
+    test.append(img.get_attribute('src'))
+print(test)
+# img_jpg = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "v4dQwb")))
+# img_link = img_jpg.get_attribute('src')
+# print(img_link)
 
-# Download the image
-pyautogui.hotkey("ctrl", "s")
-time.sleep(1)
-pyautogui.press("enter")
+#|----------------------------------------------------------------------------------------------------------|
 
-path = r'C:\Users\steam\Downloads'
-downloads = os.listdir(path)
-# for items in downloads:
 
-list = []
-for (root, dirs, file) in os.walk(path, topdown=True):
-    for f in file:
-        if '.html' in f:
-            list.append(f)
 
+# # Download the image
+# pyautogui.hotkey("ctrl", "s")
+# time.sleep(1)
+# pyautogui.press("enter")
+
+# time.sleep(10)
+
+# path = get_path(FOLDERID.Downloads)
+# downloads = os.listdir(path)
+
+# # for items in downloads:
+
+# list = []
+# for (root, dirs, file) in os.walk(path, topdown=True):
+#     for f in file:
+#         if '.html' in f:
+#             list.append(f)
+
+    
+#|----------------------------------------------------------------------------------------------------------|
+
+# maybe... ---^
--- SoupBot.py
+++ SoupBot.py
@@ -5,17 +5,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bot_TKN = os.getenv('BOT_TOKEN')
+bot_TKN = os.getenv('BOT_TOKEN') # when accessed online BOT_TOKEN comes from heroku config vars instead of local .env file
+server_NAME = os.getenv('SERVER_NAME')
+channel_NAME = os.getenv('CHANNEL_NAME')
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 #|----------------------------------------------------------------------------------------------------------|
 
-@client.event # .event is a decorator which listens to when discord opens, aka the 'client' var             
-async def on_ready(): # on_ready is a api function tht signals tht a connection with discord has been made  
+
+
+@client.event # .event is a decorator which listens to when discord opens, aka the 'client' var        
+@commands.has_permissions(send_messages=True)     
+async def on_ready(): # on_ready is a api function tht signals tht a connection with discord has been made
+
+    server = discord.utils.get(client.guilds, name = server_NAME)
+    soup_channel = discord.utils.get(server.channels, name= channel_NAME)
+
     print(f'{client.user} has connected to Discord!') # f'{}' has same function as .format
+    await soup_channel.send('https://www.inspiredtaste.net/wp-content/uploads/2018/09/Easy-Chicken-Noodle-Soup-Recipe-1200.jpg')
 
 # @client.event
 # @commands.has_permissions()
